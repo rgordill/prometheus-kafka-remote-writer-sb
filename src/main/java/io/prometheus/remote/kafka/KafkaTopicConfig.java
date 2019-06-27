5fccefccf5d07622a5eca9180631ec6660ea2483
Ramon Gordillo <ramon.gordillo@redhat.com>
2019-06-27 18:56:31 +0200

    Added Kafka classes

---
+++ src/main/java/io/prometheus/remote/kafka/KafkaTopicConfig.java
@@ -0,0 +1,34 @@
+package io.prometheus.remote.kafka;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaAdmin;
+
+@Configuration
+public class KafkaTopicConfig {
+    
+	@Autowired
+	private KafkaProducerConfig producerConfig;
+    
+    @Value(value = "${kafka.topic}")
+    private String topic;
+ 
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, producerConfig.bootstrapAddress);
+        return new KafkaAdmin(configs);
+    }
+     
+    @Bean
+    public NewTopic topic1() {
+         return new NewTopic(topic, 1, (short) 1);
+    }
+}
